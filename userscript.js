4b5a72ddf6d461cbcd6b4e3b4041305e58b87ddd
(x⋅ln(7))⁻¹ <100133857+derivativeoflog7@users.noreply.github.com>
2024-09-27 15:38:20 +0200

    whoopsie

--- userscript.js
+++ userscript.js
@@ -3,7 +3,7 @@
 // @namespace   Violentmonkey Scripts
 // @match       https://*.reddit.com/*
 // @grant       none
-// @version     1.0.2
+// @version     1.0.3
 // @author      derivativeoflog7
 // @description Disable Reddit's new auto-translate feature
 // @run-at      document-start
@@ -16,6 +16,8 @@
     'use strict';
     var reg = /(?<=[?&])tl=.+?($|&)/;
     var currUrl = window.location.href;
-    var newUrl = currUrl.replace(reg, "");
-    window.location.href = newUrl;
+    if (currUrl.match(reg)) {
+        var newUrl = currUrl.replace(reg, "");
+        window.location.href = newUrl;
+    }
 })();
