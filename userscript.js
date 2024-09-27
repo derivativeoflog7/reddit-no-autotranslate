7d88cb17bcaf40d97dd8d2a5c39c6f76c60b6c38
(x⋅ln(7))⁻¹ <100133857+derivativeoflog7@users.noreply.github.com>
2024-09-27 15:33:19 +0200

    Of course now I think of a much simpler implementation
    However, this one will leave a trailing ? at the end of the URL if tl is the only parameter, and will also remove the trailing & if tl is the last parameter and there's a trailing & after it without any parameters . This most likely won't affect anything, but if it somehow happens to be a problem I'll revert it to the old implementation

--- userscript.js
+++ userscript.js
@@ -3,7 +3,7 @@
 // @namespace   Violentmonkey Scripts
 // @match       https://*.reddit.com/*
 // @grant       none
-// @version     1.0.1
+// @version     1.0.2
 // @author      derivativeoflog7
 // @description Disable Reddit's new auto-translate feature
 // @run-at      document-start
@@ -14,19 +14,8 @@
 
 (function() {
     'use strict';
+    var reg = /(?<=[?&])tl=.+?($|&)/;
     var currUrl = window.location.href;
-    var match = currUrl.match('[?&]tl=.+?(?=&|$)') //this took me an hour; match the tl parameter and argument INCLUDING the preceding & or ? and excluding the subsequent &
-    if (match) { //check for translation parameter in url
-      var isFirstParam = match[0][0] == '?'; //check if tl is the first parameter
-      var hasMoreParams = match.index + match[0].length != currUrl.length //check if there are more parameters after tl
-      var newUrl = currUrl.slice(0, match.index); //get URL until (the tl parameter PLUS THE PREECEDING ? or &)
-      if (hasMoreParams) { //if tl is the only parameter, we don't need to do anything else
-        var restOfUrl = currUrl.slice(match.index + match[0].length + 1) //get the rest of the parameters TRUNCATING the first &
-        if (isFirstParam)
-          newUrl += '?' + restOfUrl;
-        else
-          newUrl += '&' + restOfUrl
-      }
-      window.location.href = newUrl; //redirect to new URL
-    }
+    var newUrl = currUrl.replace(reg, "");
+    window.location.href = newUrl;
 })();
