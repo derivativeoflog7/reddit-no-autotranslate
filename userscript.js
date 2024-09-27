13b7681126908e9e2c86d983ea4a8c40c73500d1
(x⋅ln(7))⁻¹ <100133857+derivativeoflog7@users.noreply.github.com>
2024-09-27 14:49:21 +0200

    Remove additional matches

--- userscript.js
+++ userscript.js
@@ -3,7 +3,7 @@
 // @namespace   Violentmonkey Scripts
 // @match       https://*.reddit.com/*
 // @grant       none
-// @version     1.0
+// @version     1.0.1
 // @author      derivativeoflog7
 // @description Disable Reddit's new idiotic auto-translate feature
 // @run-at      document-start
@@ -15,12 +15,12 @@
     var currUrl = window.location.href;
     var match = currUrl.match('[?&]tl=.+?(?=&|$)') //this took me an hour; match the tl parameter and argument INCLUDING the preceding & or ? and excluding the subsequent &
     if (match) { //check for translation parameter in url
-      var match_asFirstParam = currUrl.match('[?]tl=.+'); //check if tl is the first parameter
-      var match_hasMoreParams = currUrl.match('[?&]tl=.+[&]'); //check if there are more parameters after tl
+      var isFirstParam = match[0][0] == '?'; //check if tl is the first parameter
+      var hasMoreParams = match.index + match[0].length != currUrl.length //check if there are more parameters after tl
       var newUrl = currUrl.slice(0, match.index); //get URL until (the tl parameter PLUS THE PREECEDING ? or &)
-      if (match_hasMoreParams) { //if tl is the only parameter, we don't need to do anything else
+      if (hasMoreParams) { //if tl is the only parameter, we don't need to do anything else
         var restOfUrl = currUrl.slice(match.index + match[0].length + 1) //get the rest of the parameters TRUNCATING the first &
-        if (match_asFirstParam)
+        if (isFirstParam)
           newUrl += '?' + restOfUrl;
         else
           newUrl += '&' + restOfUrl
